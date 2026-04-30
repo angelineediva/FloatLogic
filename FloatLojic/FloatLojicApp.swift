dc0648da8380955b93a8d2c7e76dc775a2243a86
angelineedivaa <angeline.putri002@binus.ac.id>
2026-04-30 10:36:23 +0700

    Initial Commit

---
+++ FloatLojic/FloatLojicApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatLojicApp.swift
+//  FloatLojic
+//
+//  Created by Angeline on 30/04/26.
+//
+
+import SwiftUI
+
+@main
+struct FloatLojicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
